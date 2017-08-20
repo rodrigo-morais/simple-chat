d418fb7a93c9c8e31a471cc11f699ba26fa24486
rodrigo-morais <morais.rm@gmail.com>
2017-08-20 09:06:09 +0200

    Adding simple events when an user join to the chat, leaves or sends a message.

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
-var io = require('socket.io')();
+const io = require('socket.io')();
 
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+    socket.on('join', function(name) {
+      io.emit('join', `user ${name} is chatting now!`);
     });
 
-    socket.on('message', function(msg){
-      console.log('message: ' + msg);
+    socket.on('leave', function(name){
+      socket.broadcast.emit('leave', `user ${name} is disconnected`);
+    });
+
+    socket.on('message', function(name, msg){
+      socket.broadcast.emit('message', `${name}: ${msg}`);
     });
 });
 
